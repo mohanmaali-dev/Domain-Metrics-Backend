4cbabc4a67c107514975642b5d103b962ea80c4d
mohanmaali <mohan@ibrinfotech.com>
2026-08-10 12:58:54 +0530

    chnage DA

--- src/services/mozService.js
+++ src/services/mozService.js
@@ -4,7 +4,7 @@ const getStaticDomainMetrics = (domain) => {
   void domain;
 
   return {
-    domainAuthority: 80,
+    domainAuthority: 85,
     pageAuthority: 98,
     spamScore: 2,
     linkingDomains: 250000,
